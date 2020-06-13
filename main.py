65ef7866a78f0624ec9d5a58d38e73ed9b3d6da5
sunnysubmarines <sunnysubmarines@gmail.com>
2020-06-13 11:56:02 +0300

    Compare WS353 with W2V

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import Task1.parse
 from Task1.parse import Parse
 from Task2.tokenizer import Tokenizer
 from Task3.dictionaries import ContrastAnalysis
+from Task4.vectorize import Vectorize
+from Task6.word2vec import VectorizwWord2vec
 import matplotlib.pyplot as plt
 import pandas as pd
 # from scipy.cluster.hierarchy import  linkage, dendrogram
@@ -13,10 +15,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.cluster.hierarchy as shc
 
+from Task5.spam_filter import Classifier
+
 
 def task1():
-    #path1 = "Task1/txt.xml"
-    #path = "Task1/txt.txt"
+    # path1 = "Task1/txt.xml"
+    # path = "Task1/txt.txt"
     # path = "Task1/pdf.pdf"
     # path1 = "Task1/pdf.xml"
     # path = "Task1/doc.docx"
@@ -24,14 +28,14 @@ def task1():
     path = "http://2035.media/2017/12/06/demography/"
     path1 = "Task1/html.xml"
     if fnmatch.fnmatch(path, '*.txt'):
-        Parse.readtxt(path, path1)
+        Parse.read_txt(path, path1)
     elif fnmatch.fnmatch(path, '*.pdf'):
-        Parse.readPdf(path, path1)
+        Parse.read_pdf(path, path1)
         # txt = Parse.extract_text_from_pdf(path)
     elif fnmatch.fnmatch(path, '*.docx'):
-        Parse.readDocx(path, path1)
+        Parse.read_docx(path, path1)
     else:
-        Parse.readHtml(path, path1)
+        Parse.read_html(path, path1)
 
 
 
@@ -52,7 +56,6 @@ def task2():
             lev_matrix[i, j] = Tokenizer.levenshtein(l1[i], l2[j])
             jaro_matrix[i, j] = Tokenizer.jarowinkler(l1[i], l2[j])
     plt.figure(figsize=(10, 8), dpi=300)
-    plt.figure
     plt.title("Hamming")
     c_link = shc.linkage(hamming_matrix, 'complete', 'correlation')
     dend = shc.dendrogram(c_link, 30, None, None, True, 'top', labels=l1)
@@ -67,35 +70,26 @@ def task2():
     plt.savefig('jarowinkler_distance.png')
 
 
-def plot_dendrogram(model, **kwargs):
-    # Create linkage matrix and then plot the dendrogram
-
-    # create the counts of samples under each node
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  # leaf node
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
-
-    linkage_matrix = np.column_stack([model.children_, model.distances_, counts]).astype(float)
-
-    dendrogram = shc.dendrogram(linkage_matrix, **kwargs)
-    plt.show()
-
-
-
 def task3():
     folder1, folder2, folder3 = "Task3\group1", "Task3\group2", "Task3\group3"
-    #ContrastAnalysis.task([folder1, folder2, folder3])
+    # ContrastAnalysis.task([folder1, folder2, folder3])
     ContrastAnalysis.task2([folder1, folder2, folder3])
-    #ContrastAnalysis.task([ "Task3\group3"])
+    # ContrastAnalysis.task([ "Task3\group3"])
 
 
-#task1()
-#task2()
-task3()
+def task4():
+    path = "Task4/articles.csv"
+    Vectorize.task(path)
+
+
+def task5():
+    path = "Task5/sms_spam.csv"
+    Classifier.task(path)
+
+
+def task6():
+    path = "Task4/articles.csv"
+    VectorizwWord2vec.task(path)
+
+
+task6()
